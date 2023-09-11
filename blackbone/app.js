09d43d2478494ae845350667a8f0c5a0887fbbc3
imgnation1 <64916062+imgnation1@users.noreply.github.com>
2023-09-11 22:08:25 +0300

    Add files via upload

---
+++ blackbone/app.js
@@ -0,0 +1,12 @@
+// const tablet = document.querySelector(".pricelist_table");
+// const tr = document.querySelectorAll("tr");
+// console.log(tr);
+
+// // const onMouseOver = () => {
+// //   tr.forEach((item) => {
+// //     item.style.borderBottom = "#f3f3f3 1px solid";
+// //   });
+// const onMouseOver = () => {
+//   console.log("hello");
+// };
+// tablet.addEventListener("mouseover", onMouseOver);
