66f10c8ecd9f605487eb53d081622c18b839b582
kirill <kirill58010@yahoo.com>
2023-09-15 10:04:51 +0300

    dsad added points get another css for only gallery get picture :wq

--- app.js
+++ app.js
@@ -1,12 +1,63 @@
-const tablet = document.querySelector(".pricelist_table");
-const tr = document.querySelectorAll("tr");
-console.log(tr);
+const rightButton = document.querySelector(".button_right");
+const leftButton = document.querySelector(".button_left");
+let activeIndex = 0;
+const groups = document.querySelectorAll(".group");
 
-// const onMouseOver = () => {
-//   tr.forEach((item) => {
-//     item.style.borderBottom = "#f3f3f3 1px solid";
-//   });
-const onMouseOver = () => {
-  console.log("hello");
+const onRightClick = () => {
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
+
+  const currentGroup = document.querySelectorAll(
+    `[data-index="${activeIndex}"]`
+  );
+  nextGroup = document.querySelectorAll(`[data-index="${nextIndex}"]`);
+
+  // Active group becomes after
+  currentGroup.forEach((item) => {
+    item.dataset.status = "after";
+  });
+  // currentGroup.dataset.status = "after";
+  // next group becomes active
+  nextGroup.forEach((item) => {
+    item.dataset.status = "becoming-active-from-before";
+  });
+  // nextGroup.dataset.status = "becoming-active-from-before";
+
+  setTimeout(() => {
+    nextGroup.forEach((item) => {
+      item.dataset.status = "active";
+    });
+    // nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
 };
-tablet.addEventListener("mouseover", onMouseOver);
+const onLeftClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+
+  const currentGroup = document.querySelectorAll(
+      `[data-index="${activeIndex}"]`
+    ),
+    nextGroup = document.querySelectorAll(`[data-index="${nextIndex}"]`);
+  // Active group becomes after
+  currentGroup.forEach((item) => {
+    item.dataset.status = "before";
+  });
+  // currentGroup.dataset.status = "before";
+  // next group becomes active
+  nextGroup.forEach((item) => {
+    item.dataset.status = "becoming-active-from-after";
+  });
+  // nextGroup.dataset.status = "becoming-active-from-after";
+
+  setTimeout(() => {
+    nextGroup.forEach((item) => {
+      item.dataset.status = "active";
+    });
+    // nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  });
+  // console.log(activeIndex);
+};
+
+rightButton.addEventListener("click", onRightClick);
+leftButton.addEventListener("click", onLeftClick);
+console.log(document.querySelector(".main_content").parentElement);
